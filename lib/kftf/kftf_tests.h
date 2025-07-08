4a7f93e9a618e9b6e3531d9b8174695eba0fa0c9
Ethan Graham <ethangraham@google.com>
2025-07-08 18:28:40 +0000

    kftf: add test case example using annotations

--- lib/kftf/kftf_tests.h
+++ lib/kftf/kftf_tests.h
@@ -36,4 +36,47 @@ FUZZ_TEST(kftf_fuzzable, struct kftf_simple_arg)
 	kftf_fuzzable(arg.first, arg.second, arg.third);
 }
 
+struct my_fun_func_arg {
+	const char *string;
+	char *buffer;
+	size_t buffer_size;
+};
+
+static void my_fun_func(const char *string, char *buffer, size_t buffer_size)
+{
+	size_t i;
+	/* string should be NULL terminated! */
+	pr_info("this is my string: %s", string);
+
+	for (i = 0; i < buffer_size; i++) {
+		buffer[i]++;
+		pr_info("buffer[%zu] = %c\n", i, buffer[i]);
+	}
+}
+
+FUZZ_TEST(my_memncpy, struct my_fun_func_arg)
+{
+	const char *kernel_string;
+	char *kernel_buffer;
+
+	KFTF_ANNOTATE_STRING(my_fun_func_arg, string);
+	KFTF_ANNOTATE_LEN(my_fun_func_arg, buffer_size, buffer);
+	KFTF_EXPECT_NOT_NULL(my_fun_func_arg, string);
+	KFTF_EXPECT_NOT_NULL(my_fun_func_arg, buffer);
+
+	kernel_string = strndup_user(arg.string, PAGE_SIZE);
+	if (!kernel_string || IS_ERR(kernel_string))
+		return;
+
+	kernel_buffer = memdup_user(arg.buffer, arg.buffer_size);
+	if (!kernel_buffer || IS_ERR(kernel_buffer)) {
+		kfree(kernel_string);
+		return;
+	}
+
+	my_fun_func(kernel_string, kernel_buffer, arg.buffer_size);
+	kfree(kernel_string);
+	kfree(kernel_buffer);
+}
+
 #endif /* KFTF_TESTS_H */
