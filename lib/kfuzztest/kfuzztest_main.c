aeeb29fc73c204b808d3b59a42714dcf024e03fa
Ethan Graham <ethangraham@google.com>
2025-07-25 09:49:46 +0000

    kfuzztest: update header comment in kfuzztest_main.c

--- lib/kfuzztest/kfuzztest_main.c
+++ lib/kfuzztest/kfuzztest_main.c
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Kernel Fuzz Testing Framework (KFTF) - Core Module
+ * Kernel Fuzz Testing Framework (KFuzzTest) - Core Module
  *
  * This module is responsible for discovering and initializing all fuzz test
  * cases defined using the FUZZ_TEST() macro. It creates a debugfs interface
