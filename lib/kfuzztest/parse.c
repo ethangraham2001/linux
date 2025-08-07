af9d76f5a9647328189740d3277926fe6e3cb987
Ethan Graham <ethangraham@google.com>
2025-08-07 12:03:44 +0000

    kfuzztest: fix parsing logic that didn't account for padding

--- lib/kfuzztest/parse.c
+++ lib/kfuzztest/parse.c
@@ -6,12 +6,10 @@ int __kfuzztest_write_cb_common(struct file *filp, const char __user *buf,
 				size_t len, loff_t *off, void *arg,
 				size_t arg_size)
 {
-	if (len != arg_size) {
+	if (len != arg_size)
 		return -EINVAL;
-	}
-	if (simple_write_to_buffer((void *)arg, arg_size, off, buf, len) < 0) {
+	if (simple_write_to_buffer((void *)arg, arg_size, off, buf, len) < 0)
 		return -EFAULT;
-	}
 	return 0;
 }
 
@@ -20,8 +18,6 @@ int __kfuzztest_parse_input(void *input, size_t input_size,
 			    struct reloc_table **ret_reloc_table,
 			    void **ret_payload_start, void **ret_payload_end)
 {
-	pr_info("[ENTER] %s", __FUNCTION__);
-	int err;
 	void *payload_end, *payload_start;
 	size_t reloc_table_size, regions_size;
 	struct reloc_table *rt;
@@ -37,39 +33,23 @@ int __kfuzztest_parse_input(void *input, size_t input_size,
 	regions_size = sizeof(*regions) +
 		       regions->num_regions * sizeof(struct reloc_region);
 
-	pr_info("kfuzztest: num regions = %u", regions->num_regions);
-
 	rt = (struct reloc_table *)((char *)regions + regions_size);
-	if ((char *)rt > (char *)payload_end) {
-		err = -EINVAL;
-		goto fail;
-	}
+	if ((char *)rt > (char *)payload_end)
+		return -EINVAL;
 
 	reloc_table_size = sizeof(*rt) +
 			   rt->num_entries * sizeof(struct reloc_entry) +
 			   rt->payload_offset;
-	if ((char *)rt + reloc_table_size > (char *)payload_end) {
-		err = -EINVAL;
-		goto fail;
-	}
+	if ((char *)rt + reloc_table_size > (char *)payload_end)
+		return -EINVAL;
 
-	pr_info("kfuzztest: num relocations = %u, size = %zu", rt->num_entries,
-		reloc_table_size);
-
-	payload_start = (char *)(rt->entries + rt->num_entries);
-	if ((char *)payload_start > (char *)payload_end) {
-		err = -EINVAL;
-		goto fail;
-	}
-
-	pr_info("kfuzztest: payload: [ %px, %px )", payload_start, payload_end);
+	payload_start = (char *)(rt) + reloc_table_size;
+	if ((char *)payload_start > (char *)payload_end)
+		return -EINVAL;
 
 	*ret_regions = regions;
 	*ret_reloc_table = rt;
 	*ret_payload_start = payload_start;
 	*ret_payload_end = payload_end;
 	return 0;
-fail:
-	kfree(input);
-	return err;
 }
