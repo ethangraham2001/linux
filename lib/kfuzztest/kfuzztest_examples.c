1a286cb8f0186ff4d1facb523c29123cd07eb915
Ethan Graham <ethangraham@google.com>
2025-08-07 12:04:17 +0000

    kfuzztest: update examples file

--- lib/kfuzztest/kfuzztest_examples.c
+++ lib/kfuzztest/kfuzztest_examples.c
@@ -15,33 +15,17 @@ struct nested_buffers {
  *
  * | a | b | pad[8] | *a | pad[8] | *b |
  *
- * In DISTINCT mode, this will result in 3 distinct kmalloc'd regions, and
- * in POISONED mode, the buffer is untouched but the padding will be poisoned.
+ * We expect to see a KASAN warning by overflowing one byte into the A buffer.
  *
- * In this test case, we look to see that a KASAN warning is triggered in both
- * cases when overflowing on *a by one.
  */
 FUZZ_TEST(test_overflow_on_nested_buffer, struct nested_buffers)
 {
-	u32 a_size;
-	u32 b_size;
-
 	KFUZZTEST_EXPECT_NOT_NULL(nested_buffers, a);
 	KFUZZTEST_EXPECT_NOT_NULL(nested_buffers, b);
 	KFUZZTEST_ANNOTATE_LEN(nested_buffers, a_len, a);
 	KFUZZTEST_ANNOTATE_LEN(nested_buffers, b_len, b);
 
-	volatile char c;
-	pr_info("a = [%px, %px)", arg->a, arg->a + arg->a_len);
-	pr_info("b = [%px, %px)", arg->b, arg->b + arg->b_len);
-	pr_info("a_len = %zu", arg->a_len);
-	pr_info("b_len = %zu", arg->b_len);
-
-	a_size = KFUZZTEST_REGION_SIZE(1);
-	b_size = KFUZZTEST_REGION_SIZE(2);
-	pr_info("actual sizes = %u, %u", a_size, b_size);
-
 	/* Buffer overflow out of a bounds. This should be caught by KASAN. */
-	for (size_t i = 0; i <= a_size; i++)
-		c = arg->a[i];
+	for (size_t i = 0; i <= arg->a_len; i++)
+		READ_ONCE(arg->a[i]);
 }
